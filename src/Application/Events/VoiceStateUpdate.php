d41b58170e34479acd503b6c3ddd7eb380715b46
Bruno Oliveira <5279392+brunofunnie@users.noreply.github.com>
2024-03-05 15:53:08 -0300

    feat: rename redis key and format description for presence log

--- src/Application/Events/VoiceStateUpdate.php
+++ src/Application/Events/VoiceStateUpdate.php
@@ -27,16 +27,16 @@ class VoiceStateUpdate
         $channel = $data->channel;
         $user = $data->user;
         $isUserDeafened = $data->self_deaf || $data->deaf;
-        $presenceCache = $this->redis->get("presence_cache:" . $user->id);
+        $presenceCache = $this->redis->get("presence_coins:" . $user->id);
 
         // Joined a channel
         if ($channel) {
             // If there is previous data, delete it to prevent some nasty earnings.
             if ($presenceCache) {
-                $this->redis->del("presence_cache:" . $user->id);
+                $this->redis->del("presence_coins:" . $user->id);
             }
 
-            $this->redis->set("presence_cache:" . $user->id, json_encode([
+            $this->redis->set("presence_coins:" . $user->id, json_encode([
                 "entry_time" => time(),
                 "id" => $user->id
             ]));
@@ -44,7 +44,7 @@ class VoiceStateUpdate
 
         // if the user left a channel or is deaf, we remove it from the cache and pay their coins if he was there for more than 1 minute
         if ($channel == null || $isUserDeafened) {
-            $presenceCache = $this->redis->get("presence_cache:" . $user->id);
+            $presenceCache = $this->redis->get("presence_coins:" . $user->id);
 
             // If the user is not in the cache, we return
             if (!$presenceCache) {
@@ -65,7 +65,12 @@ class VoiceStateUpdate
             if ($elapsedSeconds >= 60) {
                 $coinPercentage = 5 / 100; // 5 percent
                 $accumulatedAmount = $elapsedSeconds * $coinPercentage;
-                $this->userRepository->giveCoins($user->id, $accumulatedAmount, 'Presence', "Coins por ficar " . $elapsedSeconds . " segundos em call");
+                $presenceDescription = [
+                    'amount' => $accumulatedAmount,
+                    'elapsedSeconds' => $elapsedSeconds,
+                ];
+
+                $this->userRepository->giveCoins($user->id, $accumulatedAmount, json_encode($presenceDescription));
                 $discord->getLogger()->debug(
                     sprintf(
                         "Presence: username: %s - received: %s coins - elapsed seconds: %s",
@@ -76,7 +81,7 @@ class VoiceStateUpdate
                 );
             }
 
-            $this->redis->del("presence_cache:" . $user->id);
+            $this->redis->del("presence_coins:" . $user->id);
             $discord->getLogger()->debug(
                 sprintf(
                     "Presence: username: %s - left the channel",
