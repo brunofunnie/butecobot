1177007e22a546f7581a8f86a1d36c1168da37f9
Bruno Oliveira <5279392+brunofunnie@users.noreply.github.com>
2024-04-02 19:41:11 -0300

    feat: add new image to drink water alert

--- src/config/images.php
+++ src/config/images.php
@@ -55,6 +55,7 @@ return [
             'https://chorume.tech/imgs/chorumebot/drink-water-1.gif?v0.0.1',
             'https://chorume.tech/imgs/chorumebot/drink-water-2.gif?v0.0.1',
             'https://chorume.tech/imgs/chorumebot/drink-water-3.gif?v0.0.1',
+            'https://chorume.tech/imgs/chorumebot/drink-water-4.gif?v0.0.1',
         ]
     ],
 ];
